506116e62d5e2d7e35259218587ce38077c5452e
Zulangy Satizabal <zulangy.sati15@gmail.com>
2023-08-23 12:07:40 -0500

    Avance y btnCopiar

---
+++ index.js
@@ -0,0 +1,8 @@
+let btnEncriptar = document.querySelector(".button_encrypt");
+let btnDesencriptar = document.querySelector(".button__decrypt");
+let muneico = document.querySelector(".ini_s__img");
+let contenedorParrafo = document.querySelector(".is__p");
+let resultado = document.querySelector(".out__resultado");
+
+btnEncriptar.onClick = encriptar;
+btnDesencriptar.onClick = desencriptar;
